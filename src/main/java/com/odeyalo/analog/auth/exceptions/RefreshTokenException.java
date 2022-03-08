9dfa154f1bc63ec57e48ad28bcb279eb1283aff4
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-08 17:39:57 +0200

    Exception

---
+++ src/main/java/com/odeyalo/analog/auth/exceptions/RefreshTokenException.java
@@ -0,0 +1,15 @@
+package com.odeyalo.analog.auth.exceptions;
+
+public class RefreshTokenException extends RuntimeException {
+
+    public RefreshTokenException() {
+    }
+
+    public RefreshTokenException(String message) {
+        super(message);
+    }
+
+    public RefreshTokenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

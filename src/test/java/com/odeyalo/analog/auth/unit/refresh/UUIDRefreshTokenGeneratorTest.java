f0437e4e3f3e9d8287e413b5717c0373ebe374b0
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-08 17:51:37 +0200

    Test for UUIDRefreshTokenGenerator

---
+++ src/test/java/com/odeyalo/analog/auth/unit/refresh/UUIDRefreshTokenGeneratorTest.java
@@ -0,0 +1,18 @@
+package com.odeyalo.analog.auth.unit.refresh;
+
+import com.odeyalo.analog.auth.service.refresh.UUIDRefreshTokenGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class UUIDRefreshTokenGeneratorTest {
+
+    private final UUIDRefreshTokenGenerator generator = new UUIDRefreshTokenGenerator();
+
+
+    @Test
+    void generate() {
+        String uuid = generator.generate();
+        assertNotNull(uuid);
+    }
+}

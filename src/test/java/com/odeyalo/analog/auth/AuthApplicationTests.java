020c6ec142c591d16ad6f38e28098de8dcd90847
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-08 17:23:16 +0200

    Tests

---
+++ src/test/java/com/odeyalo/analog/auth/AuthApplicationTests.java
@@ -0,0 +1,15 @@
+package com.odeyalo.analog.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
+class AuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

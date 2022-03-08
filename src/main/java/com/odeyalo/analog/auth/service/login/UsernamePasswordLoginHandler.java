669f88f67a4dd0f58133a0053d30772d3e6cc45c
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-08 17:46:12 +0200

    Login user by nickname  and password

---
+++ src/main/java/com/odeyalo/analog/auth/service/login/UsernamePasswordLoginHandler.java
@@ -0,0 +1,42 @@
+package com.odeyalo.analog.auth.service.login;
+
+import com.odeyalo.analog.auth.entity.User;
+import com.odeyalo.analog.auth.exceptions.LoginException;
+import com.odeyalo.analog.auth.exceptions.UserNotExistException;
+import com.odeyalo.analog.auth.repository.UserRepository;
+import com.odeyalo.analog.auth.service.validators.RequestUserDTOValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static java.lang.String.format;
+
+@Service
+public class UsernamePasswordLoginHandler implements LoginHandler {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final Logger logger = LoggerFactory.getLogger(UsernamePasswordLoginHandler.class);
+    private final RequestUserDTOValidator validator;
+
+    public UsernamePasswordLoginHandler(UserRepository userRepository,
+                                        PasswordEncoder passwordEncoder,
+                                        RequestUserDTOValidator validator) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.validator = validator;
+    }
+
+    @Override
+    public User login(User user) throws LoginException {
+        Optional<User> optional = this.userRepository.findUserByNickname(user.getNickname());
+        if (!optional.isPresent() || !passwordEncoder.matches(user.getPassword(), optional.get().getPassword())) {
+            this.logger.info("Error in auth process: Wrong nickname or password");
+            throw new UserNotExistException("Wrong nickname or password");
+        }
+        return optional.get();
+    }
+}

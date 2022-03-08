46bfe9d4a482c5e1d68e934f8c58de853e8e2ae8
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-08 17:42:43 +0200

    Contains email, nickname and password

---
+++ src/main/java/com/odeyalo/analog/auth/dto/RegisterUserDTO.java
@@ -0,0 +1,40 @@
+package com.odeyalo.analog.auth.dto;
+
+public class RegisterUserDTO {
+
+    private String nickname;
+    private String email;
+    private String password;
+
+    public RegisterUserDTO() {}
+
+    public RegisterUserDTO(String nickname, String email, String password) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

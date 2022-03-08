0f72d9e2b1e3538225104f7cc996d51e1e16d643
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-08 17:41:08 +0200

    Create, save, verify and delete refresh token

---
+++ src/main/java/com/odeyalo/analog/auth/service/refresh/RefreshTokenHandler.java
@@ -0,0 +1,15 @@
+package com.odeyalo.analog.auth.service.refresh;
+
+import com.odeyalo.analog.auth.entity.RefreshToken;
+import com.odeyalo.analog.auth.entity.User;
+import com.odeyalo.analog.auth.exceptions.RefreshTokenException;
+
+public interface RefreshTokenHandler {
+
+    RefreshToken createAndSaveToken(User user);
+
+    boolean verifyToken(RefreshToken refreshToken) throws RefreshTokenException;
+
+    void deleteTokenByUserId(Integer userId);
+
+}

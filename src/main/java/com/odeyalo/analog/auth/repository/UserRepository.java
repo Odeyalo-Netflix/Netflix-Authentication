5d5bee756170d1d1b52de2ad50d9131f9604b9ca
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-08 17:50:23 +0200

    User repository

---
+++ src/main/java/com/odeyalo/analog/auth/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.odeyalo.analog.auth.repository;
+
+import com.odeyalo.analog.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findUserByEmailAndPassword(String email, String password);
+
+    Optional<User> findUserByNicknameAndPassword(String nickname, String password);
+
+    Optional<User> findUserByNickname(String nickname);
+
+    Optional<User> findUserByEmail(String email);
+}

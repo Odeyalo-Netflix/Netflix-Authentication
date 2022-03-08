b1f17700368e71089e4398709a58c26965e47c25
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-08 17:22:51 +0200

    Root class

---
+++ src/main/java/com/odeyalo/analog/auth/AuthApplication.java
@@ -0,0 +1,17 @@
+package com.odeyalo.analog.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableZuulProxy
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}

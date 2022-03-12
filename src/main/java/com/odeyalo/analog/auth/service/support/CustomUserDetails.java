514f2fc48e3f9ebe3e871820866ecc369445c568
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-12 16:46:36 +0200

    Now implement Oauth2User

--- src/main/java/com/odeyalo/analog/auth/service/support/CustomUserDetails.java
+++ src/main/java/com/odeyalo/analog/auth/service/support/CustomUserDetails.java
@@ -1,23 +1,53 @@
 package com.odeyalo.analog.auth.service.support;
 
 import com.odeyalo.analog.auth.entity.User;
-import com.odeyalo.analog.auth.entity.enums.Role;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Collection;
-import java.util.Set;
+import java.util.Map;
 
-public class CustomUserDetails implements UserDetails {
-    private final User user;
+@Component
+public class CustomUserDetails implements OAuth2User, UserDetails {
+    protected  User user;
+    protected Map<String, Object> attributes;
+    private Logger logger = LoggerFactory.getLogger(CustomUserDetails.class);
+
+    public CustomUserDetails() {}
 
     public CustomUserDetails(User user) {
         this.user = user;
     }
 
+    @PostConstruct
+    public void init() {
+        this.logger.info("{CLASS LOADER}");
+        this.logger.info("{}", this.getClass().getClassLoader());
+    }
+
+    public static CustomUserDetails create(User user, Map<String, Object> attributes) {
+        CustomUserDetails details = new CustomUserDetails(user);
+        details.setAttributes(attributes);
+        return details;
+    }
+
+    public void setAttributes(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return user.getRoles();
+        return this.user.getRoles();
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
     }
 
     @Override
@@ -52,5 +82,10 @@ public class CustomUserDetails implements UserDetails {
 
     public User getUser() {
         return user;
+    }
+
+    @Override
+    public String getName() {
+        return this.getUsername();
     }
 }

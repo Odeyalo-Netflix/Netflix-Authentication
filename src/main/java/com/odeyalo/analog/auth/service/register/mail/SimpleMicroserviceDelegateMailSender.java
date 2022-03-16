6c3fd3776be6039b088256539828d77d1c6f08c0
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-16 16:30:06 +0200

    Delegate mail message sending to another microservice

---
+++ src/main/java/com/odeyalo/analog/auth/service/register/mail/SimpleMicroserviceDelegateMailSender.java
@@ -0,0 +1,28 @@
+package com.odeyalo.analog.auth.service.register.mail;
+
+import com.odeyalo.analog.auth.dto.MailMessageDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class SimpleMicroserviceDelegateMailSender implements MailSender {
+    private final RestTemplate restTemplate;
+    private final static String EMAIL_SENDER_MICROSERVICE_ENTRYPOINT = "http://localhost:8081/api/v1/send";
+    private final Logger logger = LoggerFactory.getLogger(SimpleMicroserviceDelegateMailSender.class);
+
+    public SimpleMicroserviceDelegateMailSender(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public void send(String body, String subject, String to) {
+        this.send(new MailMessageDTO(subject, body, to));
+    }
+
+    @Override
+    public void send(MailMessageDTO dto) {
+        this.restTemplate.postForObject(EMAIL_SENDER_MICROSERVICE_ENTRYPOINT, dto, String.class);
+    }
+}

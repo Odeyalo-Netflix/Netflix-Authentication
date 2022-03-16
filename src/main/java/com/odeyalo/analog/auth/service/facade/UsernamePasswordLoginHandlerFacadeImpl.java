1cd786367f899b4cf947695dfe159dcc82429305
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-16 16:30:33 +0200

    Changed constructors params

--- src/main/java/com/odeyalo/analog/auth/service/facade/UsernamePasswordLoginHandlerFacadeImpl.java
+++ src/main/java/com/odeyalo/analog/auth/service/facade/UsernamePasswordLoginHandlerFacadeImpl.java
@@ -5,7 +5,7 @@ import com.odeyalo.analog.auth.dto.response.JwtTokenResponseDTO;
 import com.odeyalo.analog.auth.entity.RefreshToken;
 import com.odeyalo.analog.auth.entity.User;
 import com.odeyalo.analog.auth.service.login.LoginHandler;
-import com.odeyalo.analog.auth.service.refresh.RefreshTokenHandler;
+import com.odeyalo.analog.auth.service.refresh.RefreshTokenProvider;
 import com.odeyalo.analog.auth.service.support.CustomUserDetails;
 import org.springframework.stereotype.Service;
 
@@ -13,20 +13,20 @@ import org.springframework.stereotype.Service;
 public class UsernamePasswordLoginHandlerFacadeImpl implements UsernamePasswordLoginHandlerFacade {
     private final LoginHandler usernamePasswordLoginHandler;
     private final JwtTokenProvider provider;
-    private final RefreshTokenHandler refreshTokenHandler;
+    private final RefreshTokenProvider refreshTokenProvider;
     public UsernamePasswordLoginHandlerFacadeImpl(LoginHandler usernamePasswordLoginHandler,
                                                   JwtTokenProvider provider,
-                                                  RefreshTokenHandler refreshTokenHandler) {
+                                                  RefreshTokenProvider refreshTokenProvider) {
         this.usernamePasswordLoginHandler = usernamePasswordLoginHandler;
         this.provider = provider;
-        this.refreshTokenHandler = refreshTokenHandler;
+        this.refreshTokenProvider = refreshTokenProvider;
     }
 
     @Override
     public JwtTokenResponseDTO login(User dto) {
         User user = this.usernamePasswordLoginHandler.login(dto);
         String jwtToken = this.provider.generateJwtToken(new CustomUserDetails(user));
-        RefreshToken refreshToken = this.refreshTokenHandler.createAndSaveToken(user);
+        RefreshToken refreshToken = this.refreshTokenProvider.createAndSaveToken(user);
         return new JwtTokenResponseDTO(true, jwtToken, refreshToken.getRefreshToken());
     }
 }

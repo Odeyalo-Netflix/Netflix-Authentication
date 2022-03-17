d93862166b46609b298914e8e445eacc185d401d
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-17 21:51:29 +0200

    Added email verification using six-number code

--- src/main/java/com/odeyalo/analog/auth/controllers/AuthController.java
+++ src/main/java/com/odeyalo/analog/auth/controllers/AuthController.java
@@ -5,42 +5,58 @@ import com.odeyalo.analog.auth.dto.RegisterUserDTO;
 import com.odeyalo.analog.auth.dto.request.RefreshTokenRequest;
 import com.odeyalo.analog.auth.dto.response.JwtTokenResponseDTO;
 import com.odeyalo.analog.auth.dto.response.RefreshTokenResponseDTO;
-import com.odeyalo.analog.auth.service.facade.UsernamePasswordLoginHandlerFacade;
-import com.odeyalo.analog.auth.service.facade.UsernamePasswordRegisterHandlerFacade;
+import com.odeyalo.analog.auth.service.facade.EmailCodeVerificationHandlerFacade;
 import com.odeyalo.analog.auth.service.facade.JwtWithRefreshTokenResponseDTOBuilder;
+import com.odeyalo.analog.auth.service.facade.login.UsernamePasswordLoginHandlerFacade;
+import com.odeyalo.analog.auth.service.facade.register.UsernamePasswordRegisterHandlerFacade;
 import com.odeyalo.analog.auth.service.support.UserConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.security.auth.message.AuthException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/auth")
 public class AuthController {
     private final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
     private final UsernamePasswordRegisterHandlerFacade registerHandler;
     private final UsernamePasswordLoginHandlerFacade loginHandler;
     private final JwtWithRefreshTokenResponseDTOBuilder jwtWithRefreshTokenResponseDTOBuilder;
+    private final EmailCodeVerificationHandlerFacade verificationHandler;
 
-    public AuthController(UsernamePasswordRegisterHandlerFacade registerHandler, UsernamePasswordLoginHandlerFacade loginHandler, JwtWithRefreshTokenResponseDTOBuilder jwtWithRefreshTokenResponseDTOBuilder) {
+    public AuthController(@Qualifier("emailVerificationUsernamePasswordRegisterHandlerFacade") UsernamePasswordRegisterHandlerFacade registerHandler,
+                          UsernamePasswordLoginHandlerFacade loginHandler,
+                          JwtWithRefreshTokenResponseDTOBuilder jwtWithRefreshTokenResponseDTOBuilder,
+                          EmailCodeVerificationHandlerFacade verificationHandler) {
         this.registerHandler = registerHandler;
         this.loginHandler = loginHandler;
         this.jwtWithRefreshTokenResponseDTOBuilder = jwtWithRefreshTokenResponseDTOBuilder;
+        this.verificationHandler = verificationHandler;
     }
 
-    @PostMapping("/auth")
-    public ResponseEntity<?> auth(@RequestBody RegisterUserDTO userDTO) throws AuthException {
+    @PostMapping("/register")
+    public ResponseEntity<?> auth(@RequestBody RegisterUserDTO userDTO, HttpServletResponse response) throws AuthException, IOException {
         JwtTokenResponseDTO jwtTokenResponseDTO = registerHandler.save(UserConverter.convertToUser(userDTO));
-        return new ResponseEntity<>(jwtTokenResponseDTO, HttpStatus.CREATED);
+//        response.sendRedirect("/api/v1/auth/verify/code");
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginUserDTO userDto) {
         JwtTokenResponseDTO jwtTokenResponseDTO = this.loginHandler.login(UserConverter.convertToUser(userDto));
         return new ResponseEntity<>(jwtTokenResponseDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/verify/code")
+    public ResponseEntity<?> codeVerify(@RequestParam String code) {
+        JwtTokenResponseDTO dto = this.verificationHandler.verifyCode(code);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
     @PostMapping("/refreshToken")

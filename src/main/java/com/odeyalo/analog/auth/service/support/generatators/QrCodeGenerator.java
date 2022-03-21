3ba0b59a3e192928082da9d396116fbd7dcd527b
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-21 16:34:48 +0200

    Generate qr code by height, width and data

---
+++ src/main/java/com/odeyalo/analog/auth/service/support/generatators/QrCodeGenerator.java
@@ -0,0 +1,22 @@
+package com.odeyalo.analog.auth.service.support.generatators;
+
+import com.google.zxing.WriterException;
+
+import java.io.IOException;
+
+public interface QrCodeGenerator {
+
+    Integer DEFAULT_WIDTH = 256;
+    Integer DEFAULT_HEIGHT = 256;
+
+    /**
+     * Return path of generated qr code
+     * @param width
+     * @param height
+     * @param data
+     * @return
+     * @throws WriterException
+     * @throws IOException
+     */
+    String generateQrCode(Integer width, Integer height, String data) throws WriterException, IOException;
+}

2fe891a8a76fa4308a95af1a4be7618b9e0e3c92
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-21 22:50:28 +0200

    Unit test for JsonQrCodeParser

---
+++ src/test/java/com/odeyalo/analog/auth/unit/service/support/parser/JsonQrCodeParserTest.java
@@ -0,0 +1,61 @@
+package com.odeyalo.analog.auth.unit.service.support.parser;
+
+import com.odeyalo.analog.auth.dto.response.QrCodeDTO;
+import com.odeyalo.analog.auth.exceptions.QrCodeNotFoundException;
+import com.odeyalo.analog.auth.service.support.parser.JsonQrCodeParser;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonQrCodeParserTest {
+    private static final String CURRENT_PATH = Paths.get(".").toAbsolutePath().normalize().toString();
+    private static final String QR_CODE_IMAGE = CURRENT_PATH + "\\src\\test\\resources\\login\\test.qrcode.png";
+    private static final String IMAGE_WITHOUT_QR_CODE = CURRENT_PATH + "\\src\\test\\resources\\login\\test.no_qrcode.jpg";
+    private final String EXPECTED_URL = "http://localhost:8761/api/v1/qrcode/verify?code=6ccdfe04-adbe-46d6-9c86-c067d2c05ba9";
+    private final String EXPECTED_TIME = "2022-03-21T12:13:35.821";
+    private final String EXPECTED_CLIENT_ID = "123";
+
+    private final JsonQrCodeParser parser = new JsonQrCodeParser();
+
+    @BeforeAll
+    static void setup() {
+    }
+
+    @Test
+    void parseQrCodeInputStream() throws IOException {
+        QrCodeDTO qrCodeDTO = this.parser.parseQrCode(new FileInputStream(QR_CODE_IMAGE));
+        assertNotNull(qrCodeDTO);
+        assertEquals(EXPECTED_CLIENT_ID, qrCodeDTO.getClientId());
+        assertEquals(EXPECTED_URL, qrCodeDTO.getData());
+        assertEquals(EXPECTED_TIME, qrCodeDTO.getTime());
+    }
+
+    @Test
+    void testParseQrCodeMultipartFile() throws IOException {
+        QrCodeDTO qrCodeDTO = this.parser.parseQrCode(new MockMultipartFile("Image", new FileInputStream(QR_CODE_IMAGE)));
+        assertNotNull(qrCodeDTO);
+        assertEquals(EXPECTED_CLIENT_ID, qrCodeDTO.getClientId());
+        assertEquals(EXPECTED_URL, qrCodeDTO.getData());
+        assertEquals(EXPECTED_TIME, qrCodeDTO.getTime());
+    }
+
+    @Test
+    void parseQrCodeInputStreamWrongImage() throws IOException {
+        assertThrows(QrCodeNotFoundException.class, () -> {
+            this.parser.parseQrCode(new FileInputStream(IMAGE_WITHOUT_QR_CODE));
+        });
+    }
+
+    @Test
+    void testParseQrCodeMultipartFileWrongImage() throws IOException {
+        assertThrows(QrCodeNotFoundException.class, () -> {
+            this.parser.parseQrCode(new MockMultipartFile("Image", new FileInputStream(IMAGE_WITHOUT_QR_CODE)));
+        });
+    }
+}

bef32eaa040b00a6a189596850a3a39b12669a25
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-23 22:08:57 +0200

    Added utility methods

--- src/test/java/com/odeyalo/analog/auth/utils/TestUtils.java
+++ src/test/java/com/odeyalo/analog/auth/utils/TestUtils.java
@@ -1,8 +1,11 @@
 package com.odeyalo.analog.auth.utils;
 
 import com.odeyalo.analog.auth.entity.User;
+import com.odeyalo.analog.auth.entity.VerificationCode;
+import com.odeyalo.analog.auth.entity.enums.AuthProvider;
 import com.odeyalo.analog.auth.entity.enums.Role;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 public class TestUtils {
@@ -11,7 +14,7 @@ public class TestUtils {
                 .email(email)
                 .password(password)
                 .nickname(nickname)
-                .banned(false).build();
+                .banned(isBanned).build();
         user.setRoles(roles);
         return user;
     }
@@ -21,9 +24,42 @@ public class TestUtils {
                 .email(email)
                 .password(password)
                 .nickname(nickname)
-                .banned(false)
+                .banned(isBanned)
                 .role(role)
                 .build();
     }
+    public static User buildUser(Integer id, String email, String nickname, String password, boolean isBanned, AuthProvider provider, String image, Role role) {
+        return User.builder()
+                .id(id)
+                .email(email)
+                .password(password)
+                .nickname(nickname)
+                .banned(isBanned)
+                .role(role)
+                .authProvider(provider)
+                .image(image)
+                .build();
+    }
+    public static User buildGeneratedUser(Integer userId) {
+        return User.builder()
+                .id(userId)
+                .email("generated@gmail.com")
+                .password("password")
+                .role(Role.USER)
+                .authProvider(AuthProvider.LOCAL)
+                .nickname("generated123")
+                .banned(false)
+                .image("")
+                .build();
+    }
 
+    public static VerificationCode buildVerificationCode(String codeValue, User user, Integer id) {
+        return VerificationCode.builder()
+                .id(id)
+                .codeValue(codeValue)
+                .user(user)
+                .expired(LocalDateTime.now().plusMinutes(3))
+                .isActivated(false)
+                .build();
+    }
 }

525bd7cc1a2252cafeccc9368ebcc9d11617eef2
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-26 15:56:34 +0200

    Changed GetMapping to PostMapping

--- src/main/java/com/odeyalo/analog/auth/controllers/QrCodeController.java
+++ src/main/java/com/odeyalo/analog/auth/controllers/QrCodeController.java
@@ -9,10 +9,7 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -36,7 +33,7 @@ public class QrCodeController {
         return IOUtils.toByteArray(Files.newInputStream(Paths.get(qrCode)));
     }
 
-    @GetMapping(value = "/verify", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/verify", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> parse(MultipartFile file) throws Exception {
         if(file == null)
             throw new FileNotPresentException("File is required");

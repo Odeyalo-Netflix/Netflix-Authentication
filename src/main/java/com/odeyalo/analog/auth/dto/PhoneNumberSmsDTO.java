98a0803095e8b101c3203134c2a1d42c2108816a
Odeyalo <theprogrammerinfuture@gmail.com>
2022-04-27 20:38:12 +0300

    Dto that contains user phone number and message body

---
+++ src/main/java/com/odeyalo/analog/auth/dto/PhoneNumberSmsDTO.java
@@ -0,0 +1,28 @@
+package com.odeyalo.analog.auth.dto;
+
+public class PhoneNumberSmsDTO {
+    private String phoneNumber;
+    private String body;
+
+    public PhoneNumberSmsDTO() {}
+    public PhoneNumberSmsDTO(String phoneNumber, String body) {
+        this.phoneNumber = phoneNumber;
+        this.body = body;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}

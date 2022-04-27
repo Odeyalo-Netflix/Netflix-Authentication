90dde09b45d513ffa8be4c29e9dac187205a7020
Odeyalo <theprogrammerinfuture@gmail.com>
2022-04-27 20:47:05 +0300

    Contains new password for user

---
+++ src/main/java/com/odeyalo/analog/auth/dto/PasswordRecoveryDTO.java
@@ -0,0 +1,13 @@
+package com.odeyalo.analog.auth.dto;
+
+public class PasswordRecoveryDTO {
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

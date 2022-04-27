3e6b3d1f31527d942d5a794ce214122f51b2c0b8
Odeyalo <theprogrammerinfuture@gmail.com>
2022-04-27 20:35:22 +0300

    Changed fields modifiers to private

--- src/test/java/com/odeyalo/analog/auth/integration/controllers/QrCodeControllerTest.java
+++ src/test/java/com/odeyalo/analog/auth/integration/controllers/QrCodeControllerTest.java
@@ -34,9 +34,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestPropertySource(locations = "classpath:application-test.properties")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class QrCodeControllerTest {
-    public static final String QR_CODE_CONTROLLER_GENERATE_ENDPOINT = "/api/v1/qrcode/generate";
-    public static final String QR_CODE_CONTROLLER_VERIFY_ENDPOINT = "/api/v1/qrcode/verify";
-    public static final String CLIENT_ID = "DM-HSJCLaNc";
+    private static final String QR_CODE_CONTROLLER_GENERATE_ENDPOINT = "/api/v1/qrcode/generate";
+    private static final String QR_CODE_CONTROLLER_VERIFY_ENDPOINT = "/api/v1/qrcode/verify";
+    private static final String CLIENT_ID = "DM-HSJCLaNc";
     private static final String CURRENT_PATH = Paths.get(".").toAbsolutePath().normalize().toString();
     private static final String IMAGE_WITH_QR_CODE = CURRENT_PATH + "\\src\\test\\resources\\login\\test.qrcode.png";
     private static final String IMAGE_WITHOUT_QR_CODE = CURRENT_PATH + "\\src\\test\\resources\\login\\test.no_qrcode.jpg";

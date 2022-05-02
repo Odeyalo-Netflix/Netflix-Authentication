6ae2f81a62fb450d4098e32f099ef19368b7a135
Odeyalo <theprogrammerinfuture@gmail.com>
2022-05-02 21:38:41 +0300

    Added updateUserPassword methods

--- src/main/java/com/odeyalo/analog/auth/repository/UserRepository.java
+++ src/main/java/com/odeyalo/analog/auth/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.odeyalo.analog.auth.repository;
 
 import com.odeyalo.analog.auth.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -18,4 +22,14 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findUserByEmail(String email);
 
     Optional<User> findUserByPhoneNumber(String phoneNumber);
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query(value = "UPDATE User u SET u.password = :password WHERE u=:user")
+    void updateUserPassword(@Param("user") User user, @Param("password") String password);
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query(value = "UPDATE User user SET user.password = :password WHERE user.email=:email")
+    void updateUserPassword(@Param("email") String email, @Param("password") String password);
 }

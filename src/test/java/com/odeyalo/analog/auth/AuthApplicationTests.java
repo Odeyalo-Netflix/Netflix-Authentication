d898bb0ca7c8c4c08c801268e20c3897133408a4
Odeyalo <theprogrammerinfuture@gmail.com>
2022-06-19 13:15:35 +0300

    Added properties check

--- src/test/java/com/odeyalo/analog/auth/AuthApplicationTests.java
+++ src/test/java/com/odeyalo/analog/auth/AuthApplicationTests.java
@@ -1,14 +1,25 @@
 package com.odeyalo.analog.auth;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
 class AuthApplicationTests {
+    @Autowired
+    Environment environment;
+    private final Logger logger = LoggerFactory.getLogger(AuthApplicationTests.class);
 
     @Test
     void contextLoads() {
+        String property = environment.getProperty("spring.datasource.url");
+        assertNotNull(property);
     }
 }

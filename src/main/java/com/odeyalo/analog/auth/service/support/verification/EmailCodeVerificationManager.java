d6cdd100786c7deed9f2db356dbb2396b1e1f0e8
Odeyalo <theprogrammerinfuture@gmail.com>
2022-06-22 21:28:48 +0300

    Fix

--- src/main/java/com/odeyalo/analog/auth/service/support/verification/EmailCodeVerificationManager.java
+++ src/main/java/com/odeyalo/analog/auth/service/support/verification/EmailCodeVerificationManager.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+
 import java.time.LocalDateTime;
 import java.util.Optional;
 

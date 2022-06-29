de924c5861d47cfdbac49add035fdc5cc6f86eb6
Odeyalo <theprogrammerinfuture@gmail.com>
2022-06-29 21:16:02 +0300

    Added table name

--- src/main/java/com/odeyalo/analog/auth/entity/VerificationCode.java
+++ src/main/java/com/odeyalo/analog/auth/entity/VerificationCode.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "verification_codes")
 public class VerificationCode {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

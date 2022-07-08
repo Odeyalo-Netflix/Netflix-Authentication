ccc8556fbe48af3a6ce37a916b58b97ec6835d0a
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-08 19:05:27 +0300

    Contains phone number

---
+++ src/main/java/com/odeyalo/analog/auth/dto/PhoneNumberMethodPasswordRecoveryDTO.java
@@ -0,0 +1,13 @@
+package com.odeyalo.analog.auth.dto;
+
+public class PhoneNumberMethodPasswordRecoveryDTO {
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}

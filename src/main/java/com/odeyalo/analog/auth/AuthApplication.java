544d09c8cd84d1c4a5dfff71e52d129da0089c53
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-12 19:09:26 +0300

    Added feign clients

--- src/main/java/com/odeyalo/analog/auth/AuthApplication.java
+++ src/main/java/com/odeyalo/analog/auth/AuthApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableEurekaClient
+@EnableFeignClients(basePackages = "com.odeyalo.support.clients")
+@PropertySource("classpath:clients.default.properties")
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);

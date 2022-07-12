cefa077e976f2b1eb104002c7448f7dd4d645201
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-12 19:40:11 +0300

    Added logging

--- src/main/java/com/odeyalo/analog/auth/service/recovery/PhoneNumberSMSPasswordRecoveryManager.java
+++ src/main/java/com/odeyalo/analog/auth/service/recovery/PhoneNumberSMSPasswordRecoveryManager.java
@@ -9,6 +9,8 @@ import com.odeyalo.analog.auth.service.sender.sms.PhoneNumberMessageSender;
 import com.odeyalo.analog.auth.service.support.PasswordRecoverySaverSupport;
 import com.odeyalo.analog.auth.service.support.generatators.CodeGenerator;
 import com.odeyalo.analog.auth.service.support.verification.CodeVerificationManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +22,7 @@ public class PhoneNumberSMSPasswordRecoveryManager implements PasswordRecoveryMa
     private final CodeVerificationManager codeVerificationManager;
     private final PasswordRecoverySaverSupport passwordRecoverySaverSupport;
     private final UserRepository userRepository;
+    private final Logger logger = LoggerFactory.getLogger(PhoneNumberSMSPasswordRecoveryManager.class);
 
     public PhoneNumberSMSPasswordRecoveryManager(
             @Qualifier("kafkaMessageBrokerMicroserviceDelegatePhoneNumberMessageSender") PhoneNumberMessageSender messageSender,
@@ -37,17 +40,20 @@ public class PhoneNumberSMSPasswordRecoveryManager implements PasswordRecoveryMa
         User user = this.userRepository.findUserByPhoneNumber(phoneNumber).orElseThrow(() -> new UserNotExistException("Presented phone number not existed"));
         VerificationCode verificationCode = this.codeVerificationManager.generateAndSave(user, CodeGenerator.DEFAULT_CODE_LENGTH, CodeVerificationManager.DEFAULT_ACTIVE_MINUTES);
         this.messageSender.sendMessage(phoneNumber, "Your code number is: " + verificationCode.getCodeValue());
+        this.logger.info("Successfully sent message to kafka broker, generated verification code was: {}, user: {}", verificationCode.getCodeValue(), user);
     }
 
     @Override
     public void changePassword(String code, String newPassword) {
         Optional<VerificationCode> optional = this.codeVerificationManager.getVerificationCodeByCodeValue(code);
         if (!optional.isPresent()) {
+            this.logger.info("Verification code is wrong: {}", code);
             throw new IncorrectResetPasswordCodeException("Recovery password code is wrong");
         }
         VerificationCode verificationCode = optional.get();
         User user = verificationCode.getUser();
         this.passwordRecoverySaverSupport.updatePassword(user, newPassword);
+        this.logger.info("Successfully updated password for user: {}", user);
     }
 
     @Override

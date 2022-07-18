37325ac0215a1d529ab6422a9e84c39c88dc0762
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-18 13:49:52 +0300

    Removed

--- src/main/java/com/odeyalo/analog/auth/service/notification/LoginDetectedNotificationSender.java
+++
@@ -1,9 +0,0 @@
-package com.odeyalo.analog.auth.service.notification;
-
-import com.odeyalo.analog.auth.entity.User;
-
-public interface LoginDetectedNotificationSender {
-
-    void notifyUser(User user);
-
-}

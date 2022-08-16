3b8eef487acbad13a8c3db23f3af317bacfc4d25
Odeyalo <theprogrammerinfuture@gmail.com>
2022-08-16 22:03:40 +0300

    Added QueryHints to enable cache to some methods

--- src/main/java/com/odeyalo/analog/auth/repository/UserRepository.java
+++ src/main/java/com/odeyalo/analog/auth/repository/UserRepository.java
@@ -4,10 +4,12 @@ import com.odeyalo.analog.auth.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.QueryHint;
 import java.util.Optional;
 
 @Repository
@@ -17,10 +19,13 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findUserByNicknameAndPassword(String nickname, String password);
 
+    @QueryHints(@QueryHint(name = org.hibernate.annotations.QueryHints.CACHEABLE, value = "true"))
     Optional<User> findUserByNickname(String nickname);
 
+    @QueryHints(@QueryHint(name = org.hibernate.annotations.QueryHints.CACHEABLE, value = "true"))
     Optional<User> findUserByEmail(String email);
 
+    @QueryHints(@QueryHint(name = org.hibernate.annotations.QueryHints.CACHEABLE, value = "true"))
     Optional<User> findUserByPhoneNumber(String phoneNumber);
 
     @Modifying(clearAutomatically = true)

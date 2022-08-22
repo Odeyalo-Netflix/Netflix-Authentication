e3d7b540e1613abb2f28d7dc56f26989dee10cfe
Odeyalo <theprogrammerinfuture@gmail.com>
2022-08-22 13:33:16 +0300

    Event that triggers when user logged in

--- src/main/java/com/odeyalo/analog/auth/service/events/login/UserLoggedInEvent.java
+++ src/main/java/com/odeyalo/analog/auth/service/events/login/UserLoggedInEvent.java
@@ -1,14 +1,16 @@
-package com.odeyalo.analog.auth.service.events;
+package com.odeyalo.analog.auth.service.events.login;
 
 import com.odeyalo.analog.auth.entity.User;
+import com.odeyalo.analog.auth.service.events.Event;
 
 import java.time.LocalDateTime;
 
-public class UserLoggedInEvent implements Event {
-    private User user;
+public class UserLoggedInEvent extends Event {
     private LocalDateTime time; // When user logged in
+    private User user;
 
     public UserLoggedInEvent(User user, LocalDateTime time) {
+        super("USER_LOGGED_IN");
         this.user = user;
         this.time = time;
     }
@@ -27,10 +29,5 @@ public class UserLoggedInEvent implements Event {
 
     public void setTime(LocalDateTime time) {
         this.time = time;
-    }
-
-    @Override
-    public EventType getEventType() {
-        return EventType.USER_LOGGED_IN;
     }
 }

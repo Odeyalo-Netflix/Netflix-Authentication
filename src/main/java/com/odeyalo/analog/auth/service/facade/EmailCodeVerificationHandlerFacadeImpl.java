4f692817aed3774576ea6b0dafe4249c3985ba6f
Odeyalo <theprogrammerinfuture@gmail.com>
2022-08-23 14:21:14 +0300

    Added  eventHandlerManager to fire events when user is successful registered

--- src/main/java/com/odeyalo/analog/auth/service/facade/EmailCodeVerificationHandlerFacadeImpl.java
+++ src/main/java/com/odeyalo/analog/auth/service/facade/EmailCodeVerificationHandlerFacadeImpl.java
@@ -6,7 +6,9 @@ import com.odeyalo.analog.auth.entity.RefreshToken;
 import com.odeyalo.analog.auth.entity.User;
 import com.odeyalo.analog.auth.entity.VerificationCode;
 import com.odeyalo.analog.auth.exceptions.CodeVerificationException;
-import com.odeyalo.analog.auth.repository.UserRepository;
+import com.odeyalo.analog.auth.service.events.EventHandlerManager;
+import com.odeyalo.analog.auth.service.events.register.AbstractUserRegisteredEventHandler;
+import com.odeyalo.analog.auth.service.events.register.UserRegisteredEvent;
 import com.odeyalo.analog.auth.service.refresh.RefreshTokenProvider;
 import com.odeyalo.analog.auth.service.support.CustomUserDetails;
 import com.odeyalo.analog.auth.service.support.verification.CodeVerificationManager;
@@ -23,11 +25,15 @@ public class EmailCodeVerificationHandlerFacadeImpl implements EmailCodeVerifica
     private final CodeVerificationManager verificationManager;
     private final JwtTokenProvider jwtTokenProvider;
     private final RefreshTokenProvider refreshTokenProvider;
+    private final EventHandlerManager eventHandlerManager;
 
-    public EmailCodeVerificationHandlerFacadeImpl(@Qualifier("emailCodeVerificationManager") CodeVerificationManager verificationManager, JwtTokenProvider jwtTokenProvider, RefreshTokenProvider refreshTokenProvider) {
+    public EmailCodeVerificationHandlerFacadeImpl(@Qualifier("emailCodeVerificationManager") CodeVerificationManager verificationManager,
+                                                  JwtTokenProvider jwtTokenProvider, RefreshTokenProvider refreshTokenProvider,
+                                                  @Qualifier("asyncEventHandlerManager") EventHandlerManager eventHandlerManager) {
         this.verificationManager = verificationManager;
         this.jwtTokenProvider = jwtTokenProvider;
         this.refreshTokenProvider = refreshTokenProvider;
+        this.eventHandlerManager = eventHandlerManager;
     }
 
     @Override
@@ -35,13 +41,11 @@ public class EmailCodeVerificationHandlerFacadeImpl implements EmailCodeVerifica
         Optional<VerificationCode> codeOptional = this.verificationManager.getVerificationCodeByCodeValue(codeValue);
         VerificationCode code = codeOptional.orElseThrow(() -> new CodeVerificationException("Presented code is wrong"));
         User user = code.getUser();
-        System.out.println(user);
         this.deleteUsedCode(code);
         this.activateUser(user);
-        System.out.println(user);
         String jwtToken = this.jwtTokenProvider.generateJwtToken(new CustomUserDetails(user));
-        System.out.println(jwtToken);
         RefreshToken refreshToken = this.refreshTokenProvider.createAndSaveToken(user);
+        this.eventHandlerManager.notifySpecialEventHandlers(AbstractUserRegisteredEventHandler.USER_REGISTERED_EVENT_VALUE, new UserRegisteredEvent(user));
         return new JwtTokenResponseDTO(true, jwtToken, refreshToken.getRefreshToken());
     }
 

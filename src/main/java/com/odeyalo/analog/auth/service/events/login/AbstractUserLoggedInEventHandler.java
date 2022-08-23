402a4cd61bc88c5667996d306d9d830d62f34066
Odeyalo <theprogrammerinfuture@gmail.com>
2022-08-23 14:20:26 +0300

    Added checkIncomingEvent

--- src/main/java/com/odeyalo/analog/auth/service/events/login/AbstractUserLoggedInEventHandler.java
+++ src/main/java/com/odeyalo/analog/auth/service/events/login/AbstractUserLoggedInEventHandler.java
@@ -1,5 +1,6 @@
 package com.odeyalo.analog.auth.service.events.login;
 
+import com.odeyalo.analog.auth.service.events.Event;
 import com.odeyalo.analog.auth.service.events.EventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,19 @@ import org.slf4j.LoggerFactory;
 public abstract class AbstractUserLoggedInEventHandler implements EventHandler {
     public static final String USER_LOGGED_IN_EVENT_VALUE = "USER_LOGGED_IN";
     protected final Logger logger = LoggerFactory.getLogger(AbstractUserLoggedInEventHandler.class);
+
+    @Override
+    public boolean checkIncomingEvent(Event event) {
+        if (!(event.getEventType().equals(USER_LOGGED_IN_EVENT_VALUE))) {
+            this.logger.error("Wrong event was received. Expected event: {}, event type that was received: {}", USER_LOGGED_IN_EVENT_VALUE, event.getEventType());
+            return false;
+        }
+        if (!(event instanceof UserLoggedInEvent)) {
+            this.logger.error("Wrong event class was received. Excepted class: {}, received class: {}", UserLoggedInEvent.class.getName(), event.getClass().getName());
+            return false;
+        }
+        return true;
+    }
 
     @Override
     public String getEventType() {

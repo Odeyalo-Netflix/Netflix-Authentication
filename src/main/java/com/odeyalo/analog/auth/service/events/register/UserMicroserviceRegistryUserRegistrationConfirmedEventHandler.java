a07e55d7bd8d1fa3059a7031d73bf92d925413c2
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-07 20:54:08 +0300

    Changed event type

--- src/main/java/com/odeyalo/analog/auth/service/events/register/UserMicroserviceRegistryUserRegistrationConfirmedEventHandler.java
+++ src/main/java/com/odeyalo/analog/auth/service/events/register/UserMicroserviceRegistryUserRegistrationConfirmedEventHandler.java
@@ -16,11 +16,11 @@ import java.util.stream.Collectors;
  * Registry user entity in user microservice using kafka
  */
 @Service
-public class UserMicroserviceRegistryUserRegisteredEventHandler extends AbstractUserRegisteredEventHandler {
+public class UserMicroserviceRegistryUserRegistrationConfirmedEventHandler extends AbstractUserRegistrationConfirmedEventHandler {
     private final KafkaTemplate<String, UserRegisteredDTO> template;
 
     @Autowired
-    public UserMicroserviceRegistryUserRegisteredEventHandler(KafkaTemplate<String, UserRegisteredDTO> template) {
+    public UserMicroserviceRegistryUserRegistrationConfirmedEventHandler(KafkaTemplate<String, UserRegisteredDTO> template) {
         this.template = template;
     }
 
@@ -30,7 +30,7 @@ public class UserMicroserviceRegistryUserRegisteredEventHandler extends Abstract
         if (!isEventCorrect) {
             return;
         }
-        UserRegisteredEvent userRegisteredEvent = (UserRegisteredEvent) event;
+        UserRegistrationConfirmedEvent userRegisteredEvent = (UserRegistrationConfirmedEvent) event;
         User user = userRegisteredEvent.getUser();
         UserInformationDTO info = buildUserInformationDTO(user);
         this.template.send("USER_REGISTERED_TOPIC", new UserRegisteredDTO(info));
@@ -49,6 +49,7 @@ public class UserMicroserviceRegistryUserRegisteredEventHandler extends Abstract
                 .image(user.getImage())
                 .build();
     }
+
     protected Set<Role> convertToDtoRoles (Set<com.odeyalo.analog.auth.entity.enums.Role> roles) {
         return roles.stream().map(x -> Role.valueOf(x.name())).collect(Collectors.toSet());
     }
